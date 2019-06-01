36f32015569da5fe8951083aeaaf02e3e73cbcac
Luan Orlandi <orlandi.luan@gmail.com>
2019-06-01 11:50:31 -0300

    Eslint fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -12,12 +13,16 @@ const IndexPage = ({ data }) => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
+
+IndexPage.propTypes = {
+  data: PropTypes.array,
+};
 
 export const query = graphql`
   query {
@@ -29,6 +34,6 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 
-export default IndexPage
+export default IndexPage;
